08e5aa86ac172ca21b99d9a6c0d8034e3418662b
Akin94 <akin.tutkan@gmail.com>
2022-12-14 02:10:26 +0300

    Created Home Social Data Components

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/akintutkan/" className="home__social-icon" target="_blank" rel="noreferrer"> <i class="uil uil-instagram"></i> </a>
+        <a href="https://twitter.com/Akintutkan" className="home__social-icon" target="_blank" rel="noreferrer"> <i class="uil uil-twitter"></i></a>
+        <a href="https://github.com/Akintutkan" className="home__social-icon" target="_blank" rel="noreferrer"> <i class="uil uil-github-alt"></i></a>
+        <a href="https://www.linkedin.com/in/ahmet-akin-tutkan/" className="home__social-icon" target="_blank" rel="noreferrer"> <i class="uil uil-linkedin"></i></a>
+
+    </div>
+  )
+}
+
+export default Social
