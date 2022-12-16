22652262d7170ce7cf4abb5721b1bc8bfd08457d
Akin94 <akin.tutkan@gmail.com>
2022-12-17 00:25:35 +0300

    Created footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Ahmet Akın</h1>
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" className="footer__link">Projects</a>
+                </li>
+
+                <li>
+                    <a href="#contact" className="footer__link">Contact</a>
+                </li>
+            </ul>
+
+            <div className="footer__social">   
+             <a href="https://www.instagram.com/akintutkan/" className="footer__social-link" target="_blank" rel="noreferrer"> <i class="bx bxl-instagram"></i> </a>
+        <a href="https://twitter.com/Akintutkan" className="footer__social-link" target="_blank" rel="noreferrer"> <i class="bx bxl-twitter"></i></a>
+        <a href="https://github.com/Akintutkan" className="footer__social-link" target="_blank" rel="noreferrer"> <i class="bx bxl-github-alt"></i></a>
+        <a href="https://www.linkedin.com/in/ahmet-akin-tutkan/" className="footer__social-link" target="_blank" rel="noreferrer"> <i class="bx bxl-linkedin"></i></a>
+</div>
+<span className='footer__copy'>&#160; AhmetAkınTutkan. All rights reserved</span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
