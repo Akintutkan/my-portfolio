a1958148484e3e4bc5e4a966f58922cce400b623
Akin94 <akin.tutkan@gmail.com>
2022-12-20 01:02:47 +0300

    add Work component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/Home"
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Qualification from './components/qualification/Qualification';
+import Work from './components/work/Work';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Scroll from './components/scroll/Scroll';
@@ -18,8 +19,8 @@ const App = () => {
     <About />
     <Skills />
     <Qualification />
+    <Work />
     <Contact />
-    
     </main>
     <Footer />
     <Scroll />
