297e107d3332d70649a5d457a3effd8ce50aa7a6
Akin94 <akin.tutkan@gmail.com>
2023-03-12 03:00:20 +0300

    Add new site and changed qualification.jsx

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,80 +1,124 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./qualification.css";
 
 const Qualification = () => {
-    const[toggleState,setToggleState] = useState(1);
-    const toggleTab = (index) =>{
-        setToggleState(index)
-    };
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
       <span className="section__subtitle">My Personel Journey</span>
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className={toggleState === 1 ? "qualification__button qualification__active button--flex": "qualification__button button--flex"}
-          onClick={()=> toggleTab(1)}>
-            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}Education
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__content-active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+            Education
           </div>
-          <div className={toggleState === 2 ? "qualification__button qualification__active button--flex": "qualification__button button--flex"}
-          onClick={()=> toggleTab(2)}>
-            <i className="uil uil-briefcase-alt qualification__icon"></i>Experience
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__content-active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
           </div>
         </div>
         <div className="qualification__sections">
-          <div className={toggleState ===1 ? "qualification__content qualification__content-active":"qualification__content"}>
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            {/* Experience */}
             <div className="qualification__data">
-                <div>
-              <h3 className="qualification__title">FrontEnd Development</h3>
-              <span className="qualification__subtitle">BilgeAdam Academy</span>
-              <div className="qualification__calendar">
-                <i className="uil uil-calendar-alt"></i>2022-Present
+              <div></div>
+              {/* div is space */}
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">İntern-Junior Frontend Developer</h3>
+                <span className="qualification__subtitle">
+                  Kartega Innovative
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2022.09-2023.02
+                </div>
               </div>
             </div>
-            <div>
-              <span className="qualification__rounder">
-              </span>
-
-              <span className="qualification__line"></span>
-            </div>
-            </div>
-
- {/* Experience */}
+            {/* Education */}
             <div className="qualification__data">
-                <div></div>
-                <div>
-              <span className="qualification__rounder"></span>
-              <span className="qualification__line"></span>
-            </div>
-
-                <div>
-              <h3 className="qualification__title">QA/QC Engineer</h3>
-              <span className="qualification__subtitle">Basaksehir City Hospital</span>
-              <div className="qualification__calendar">
-                <i className="uil uil-calendar-alt"></i>2018-2021
+              <div>
+                <h3 className="qualification__title">FrontEnd Development</h3>
+                <span className="qualification__subtitle">
+                  BilgeAdam Academy
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2022.04-2023.01
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
               </div>
             </div>
-            </div>
 
-          
-            
-
+            {/* Experience */}
             <div className="qualification__data">
-                <div>
-              <h3 className="qualification__title">Geopyhsical Engineering</h3>
-              <span className="qualification__subtitle">Istanbul University</span>
-              <div className="qualification__calendar">
-                <i className="uil uil-calendar-alt"></i>2012-2017
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
               </div>
+
+              <div>
+                <h3 className="qualification__title">QA/QC Engineer</h3>
+                <span className="qualification__subtitle">
+                Basaksehir City Hospital
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2018.05-2021.10
+                </div>
+              </div>
+
             </div>
-            <div>
-              <span className="qualification__rounder"></span>
-              <span className="qualification__line"></span>
-            </div>
-            </div>
-            </div>
+            {/* Education */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Geopyhsical Engineering
+                </h3>
+                <span className="qualification__subtitle">
+                  Istanbul University
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>2012-2017
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,10 +1,12 @@
 import Work1 from "../../assets/work1.jpg"
 import Work2 from "../../assets/work2.jpg"
 import Work3 from "../../assets/work3.png"
-import Work4 from "../../assets/work4.jpg"
 import Work5 from "../../assets/work5.jpg"
 import Work6 from "../../assets/work6.jpg"
 import Work7 from "../../assets/work7.jpg"
+import Work8 from "../../assets/work8.jpg"
+import Work4 from "../../assets/work4.jpg"
+
 
 export const projectsData = [
     {
@@ -31,27 +33,34 @@ export const projectsData = [
     {
       id: 4,
       image: Work4,
+      title: "Cw Calculate",
+      category: "react",
+      link: "https://cw-hesaplama.netlify.app/"
+    },
+    {
+      id: 5,
+      image: Work5,
       title: "Pig Game",
       category: "javascript",
       link: "https://piggameexample.netlify.app/"
     },
     {
-      id: 5,
-      image: Work5,
+      id: 6,
+      image: Work6,
       title: "Monster Cats",
       category: "react",
       link: "https://monstercats.netlify.app/"
     },
     {
-      id: 6,
-      image: Work6,
+      id: 7,
+      image: Work7,
       title: "Country-App",
       category: "javascript",
       link: "https://country-neighbour.netlify.app/"
     },
     {
-        id: 7,
-        image: Work7,
+        id: 8,
+        image: Work8,
         title: "Add User",
         category: "react",
         link: "https://user-add.netlify.app/"
